b5b0492ff376326067e2be3e4423c2f1b8f3f4f7
naseemahamed007 <naseemishere0@gmail.com>
2026-03-08 16:30:19 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "./components/HeroSection";
+import ServicesSection from "./components/ServicesSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <ServicesSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
