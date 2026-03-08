08afd9176c0707fb1445356ce0d28706ccff6909
naseemahamed007 <naseemishere0@gmail.com>
2026-03-08 16:29:53 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: 20, background: "#111" }}>
+      &copy; 2026 Nas Tech Innovations. All Rights Reserved.
+    </footer>
+  );
+}
